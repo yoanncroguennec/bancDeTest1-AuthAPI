d057e88d84d2cfbd9e30037d146410c03913faae
sido69 <yoann.croguennec@gmail.com>
2023-03-06 14:01:21 +0100

    first commit

--- app/app.js
+++ app/app.js
@@ -13,7 +13,11 @@ const app = express();
 
 // MIDDLEWARES
 app.use(express.json());
-app.use(cors())
+const corsOptions = {
+    origin: true, //included origin as true
+    credentials: true, //included credentials as true
+};
+app.use(cors(corsOptions));
 app.use(bodyParser.urlencoded({ extended: true }));
 // For auth
 app.use(cookieParser())
